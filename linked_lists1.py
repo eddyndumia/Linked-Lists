2e6fe9c81ddcbf3652851d1c25ab6229d6c475dc
Eddy Ndumia <ndumiaeddy@gmail.com>
2023-02-15 16:33:58 +0300

    Linked Lists Insertion with Python

--- linked_lists1.py
+++ linked_lists1.py
@@ -24,5 +24,31 @@ singlelinkedlist.head.next = node2
 singlelinkedlist.tail = node2
 
 
-
-""
+def insert (self, value, location):
+    newNode = Node(value)
+    
+    if self.head is None:
+        self.head = newNode 
+        self.tail = newNode
+    else:
+        if location is 0:
+            newNode.next = self.head 
+            self.head = newNode
+        elif location is 1:
+            
+            # at the end of the linkedlist
+            # 1. update newnode reference to null as it is the last one on the list
+            newNode.next = None
+            # 2. 
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
