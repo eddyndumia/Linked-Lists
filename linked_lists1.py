333e5c59f184b08f0050951174c6d071f7127875
Eddy Ndumia <ndumiaeddy@gmail.com>
2023-02-15 13:51:30 +0300

    Linked Lists.py

---
+++ linked_lists1.py
@@ -0,0 +1,24 @@
+
+
+
+
+class Node:
+    def __init__(self, value=None):
+        
+        self.value = value
+        self.next = None
+        
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+singlelinkedlist = SLinkedList()
+
+node1 = Node(1)
+node2 = Node(2)
+
+singlelinkedlist.head = node1
+singlelinkedlist.head.next = node2
+singlelinkedlist.tail = node2
